82113713d5bc277daa5e0bfd946325ab890c4971
jair1515 <135368945+jair1515@users.noreply.github.com>
2025-06-22 06:49:03 +0000

    Aula 04 modula 02

--- game_js_main/app.js
+++ game_js_main/app.js
@@ -1,1 +1,11 @@
 alert('Boa Vindas ao jogo do numero secreto!');
+let numeroSecreto = 5;
+console.log(`O numero secreto é: ${numeroSecreto}`);
+let chute = prompt('Escolha um numero entre 1 e 30');
+
+// se o chute for igual ao numero secreto
+if (chute == numeroSecreto) {
+    alert(`Isso ai!  Você descobriu o numero secreto que é ${numeroSecreto}`);
+} else {
+    alert('Você errou :( ')
+}
