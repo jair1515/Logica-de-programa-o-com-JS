346267471b0109476252a09ba50574038ab54823
jair1515 <135368945+jair1515@users.noreply.github.com>
2025-06-23 05:07:13 +0000

    Hora da pratica at 09

---
+++ Pratica/ex_02_ao_05.js
@@ -0,0 +1,23 @@
+numero = prompt('Digite um positivo ou negativo');
+if (numero > 0) {
+    alert('Número positivo!');
+} else {
+    alert('Número negativo!');
+}
+
+// ex 3
+
+pontuacao = 105;
+if (pontuacao >= 100) {
+    console.log('Parabéns, você venceu!');
+} else {
+    console.log('Tente novamente para ganhar.');
+}
+
+// ex 04
+ let saldoConta = 500; // Exemplo de saldo
+alert(`Seu saldo é de R$${saldoConta}.`);
+
+// ex 05
+let nome = prompt('Qual o seu nome?');
+alert(`Boas vindas ${nome}`);
