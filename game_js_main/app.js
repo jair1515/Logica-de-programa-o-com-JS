6635e2ef6d14a0f8b3d0037bec41881d093b3028
jair1515 <135368945+jair1515@users.noreply.github.com>
2025-06-24 04:13:55 +0000

    Finalizado modulo 3

--- game_js_main/app.js
+++ game_js_main/app.js
@@ -1,19 +1,23 @@
 alert('Boa Vindas ao jogo do numero secreto!');
 let numeroSecreto = 5;
 console.log(`O numero secreto é: ${numeroSecreto}`);
-let chute;  
+let chute;
+let tentativas = 1;
 
 // enquanto
 while (chute != numeroSecreto) {
     chute = prompt('Escolha um numero entre 1 e 30');
     // se o chute for igual ao numero secreto
     if (chute == numeroSecreto) {
-        alert(`Isso ai!  Você descobriu o numero secreto que é ${numeroSecreto}`);
+        alert(`Isso ai!  Você descobriu o numero secreto que é ${numeroSecreto} com ${tentativas} tentativas`);
     } else if (chute > numeroSecreto){
         alert(`O Numero Secreto é menor que ${chute}`);
     } else if (chute < numeroSecreto) {
         alert (`O Numero Secreto é maior que ${chute}`);
     }
+    // tentativas = tentativas + 1;
+    tentativas ++;
+
 }
 
 
