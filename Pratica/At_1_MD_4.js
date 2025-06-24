6120a839216e08d1134e2267c2b95c25e5db63da
jair1515 <135368945+jair1515@users.noreply.github.com>
2025-06-24 06:20:11 +0000

    modulo 4

---
+++ Pratica/At_1_MD_4.js
@@ -0,0 +1,6 @@
+//let nome = 'Jair Pontes'
+//console.log(`Boas vindas ao ${nome}`) exercicio 01 & 02 modulo 04
+//alert(`Olá ${nome}`exercicio 03 modulo 04
+
+//let linguagem = prompt('Qual a linguagem de programação que você mais gosta?');
+//console.log(`Que legal voce gosta de: ${linguagem}`) ex 05
--- game_js_main/app.js
+++ game_js_main/app.js
@@ -1,15 +1,15 @@
 alert('Boa Vindas ao jogo do numero secreto!');
-let numeroSecreto = 5;
+let numeroSecreto = parseInt(Math.random() * 100 + 1);
 console.log(`O numero secreto é: ${numeroSecreto}`);
 let chute;
 let tentativas = 1;
 
 // enquanto
 while (chute != numeroSecreto) {
-    chute = prompt('Escolha um numero entre 1 e 30');
+    chute = prompt('Escolha um numero entre 1 e 100');
     // se o chute for igual ao numero secreto
     if (chute == numeroSecreto) {
-        alert(`Isso ai!  Você descobriu o numero secreto que é ${numeroSecreto} com ${tentativas} tentativas`);
+        break;
     } else if (chute > numeroSecreto){
         alert(`O Numero Secreto é menor que ${chute}`);
     } else if (chute < numeroSecreto) {
@@ -19,5 +19,15 @@ while (chute != numeroSecreto) {
     tentativas ++;
 
 }
+
+
+let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+alert(`Isso ai!  Você descobriu o numero secreto que é ${numeroSecreto} com ${tentativas} ${palavraTentativa}.`);
+
+/*if (tentativas > 1){
+alert(`Isso ai!  Você descobriu o numero secreto que é ${numeroSecreto} com ${tentativas} tentativas`);
+} else {
+    alert(`Isso ai!  Você descobriu o numero secreto que é ${numeroSecreto} com ${tentativas} tentativa`);
+}*/ 
 
 
